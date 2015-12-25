7bc2d4b9dd56c563bd47c76c4fb5f02cbe4100b2
karan1149 <karan1149@gmail.com>
2015-12-24 23:53:14 -0500

    Added error system to form validations.
    Modified edit and submit post forms to implement new error system and
    validate input.

--- client/helpers/errors.js
+++ client/helpers/errors.js
@@ -2,3 +2,10 @@ Errors = new Mongo.Collection(null); // no server name
 throwError = function(message) {
   Errors.insert({message: message});
 };
+
+Template.error.onRendered(function() {
+  var error = this.data;
+  Meteor.setTimeout(function () {
+    Errors.remove(error._id);
+  }, 3000);
+});
--- client/templates/posts/post-edit.js
+++ client/templates/posts/post-edit.js
@@ -2,17 +2,19 @@ Template.postEdit.events({
   'submit form': function(e) {
     e.preventDefault();
 
-    var currentPostId = this._id;
 
     var postProperties = {
-      url: $(e.target).find('[name=url]').val(),
-      title: $(e.target).find('[name=title]').val()
+      url: e.target.url.value,
+      title: e.target.title.value
     }
+    var errors = validatePost(postProperties);
+if (errors.title || errors.url)
+  return Session.set('postEditErrors', errors);
 
-    Posts.update(currentPostId, {$set: postProperties}, function(error) {
+    Posts.update(this._id, {$set: postProperties}, function(error) {
       if (error) {
         // display the error to the user
-        alert(error.reason);
+        throwError(error.reason);
       } else {
         Router.go('postPage', {_id: currentPostId});
       }
@@ -27,5 +29,16 @@ Template.postEdit.events({
       Posts.remove(currentPostId);
       Router.go('postsList');
     }
+  }
+});
+Template.postEdit.onCreated(function() {
+  Session.set('postEditErrors', {});
+});
+Template.postEdit.helpers({
+  errorMessage: function(field) {
+    return Session.get('postEditErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('postEditErrors')[field] ? 'has-error' : '';
   }
 });
