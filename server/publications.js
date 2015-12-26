d419539480218d17da2216e0249d98249831b611
karan1149 <karan1149@gmail.com>
2015-12-26 14:09:05 -0500

    Subscribe and provide named data context at route level.

--- server/publications.js
+++ server/publications.js
@@ -1,5 +1,13 @@
-Meteor.publish('posts', function() {
-  return Posts.find();
+Meteor.publish('posts', function(options) {
+  check(options, {
+    sort: Object,
+    limit: Number
+  });
+  return Posts.find({}, options);
+});
+Meteor.publish('onePost', function(_id) {
+  check(_id, String)
+  return Posts.find(_id);
 });
 Meteor.publish('comments', function(id) {
   check(id, String);
